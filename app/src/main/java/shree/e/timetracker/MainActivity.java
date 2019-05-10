aa6ca7212015a23e49955a93c415f255f8344054
ShriramSuryawanshi <shriram.a.suryawanshi@gmail.com>
2019-05-10 01:00:46 -0400

    Checkpoint: Using SharedPreferences as local DB - If app being used for first time, create account for user, else login to existing account

--- app/src/main/java/shree/e/timetracker/MainActivity.java
+++ app/src/main/java/shree/e/timetracker/MainActivity.java
@@ -1,5 +1,6 @@
 package shree.e.timetracker;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +9,10 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String MY_PREFS_NAME = "ShriramSuryawanshi";
+    SharedPreferences.Editor editor;
+    SharedPreferences prefs;
+
     EditText editTextUsername;
     EditText editTextPassword;
 
@@ -15,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+        editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
 
         editTextUsername = findViewById(R.id.editTextUsername);
         editTextPassword = findViewById(R.id.editTextPassword);
@@ -25,6 +33,32 @@ public class MainActivity extends AppCompatActivity {
         final String username = editTextUsername.getText().toString();
         final String password = editTextPassword.getText().toString();
 
-        Toast.makeText(this, "User - " + username + ", Pass - " + password, Toast.LENGTH_LONG).show();
+        if(username.compareTo("")== 0)            Toast.makeText(this, "Error! Username can not be blank.", Toast.LENGTH_LONG).show();
+        else if(password.compareTo("") == 0)      Toast.makeText(this, "Error! Password can not be blank.", Toast.LENGTH_LONG).show();
+        else {
+
+            String savedUser = prefs.getString("savedUser", "");
+            String savedPass = prefs.getString("savedPass", "");
+
+            if (savedUser.compareTo("") == 0) {
+                    editor.putString("savedUser", username);
+                    editor.putString("savedPass", password);
+                    editor.apply();
+                    Toast.makeText(this, "Welcome to TimeTracker!", Toast.LENGTH_LONG).show();
+                    // Show add tasklist page
+            }
+            else {
+
+                if(username.compareTo(savedUser) == 0 && password.compareTo(savedPass) == 0) {
+                    Toast.makeText(this, "Welcome back to TimeTracker!", Toast.LENGTH_LONG).show();
+                    // Show main window
+                }
+                else {
+                    editTextUsername.setText("");
+                    editTextPassword.setText("");
+                    Toast.makeText(this, "Error! Invalid login credentials! Try again!", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
     }
 }
